37430ce42923ac769d23d7c40b06b9c7d6f46f25
rikkapro0128 <stkssc002@gmail.com>
2023-02-09 16:56:51 +0700

    save debug view log eeprom

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ String GEN_ID_BY_MAC = String(getMac);
 String ID_DEVICE;
 String TYPE_DEVICE = "LOGIC";
 bool STATUS_PIN = false;
-String DATABASE_URL = "esp8266-device-db-default-rtdb.firebaseio.com";
+String DATABASE_URL = "";
 uint32_t ramSize;
 float_t flashSize = 80000;
 float_t percent = 100;
@@ -38,6 +38,9 @@ void setupWifiModeStation();
 void setupWebserverModeAP();
 
 // FUNCTION PROTOTYPE - COMPONENT
+#ifdef _DEBUG_
+void viewEEPROM();
+#endif
 void scanListNetwork();
 void setUpPinMode();
 void maybeSwitchMode();
@@ -52,11 +55,13 @@ class EepromMiru
 
 public:
   String DATABASE_NODE = "";
+  bool canAccess = false;
 
-  EepromMiru(int size)
+  EepromMiru(int size, String url_database = "")
   {
     this->size = size;
     this->updateDatabaseNode(this->readUserID(), this->readNodeID());
+    this->databaseUrl = url_database;
   }
 
   void resetAll()
@@ -101,6 +106,13 @@ public:
     this->updateDatabaseNode(this->readUserID(), node_id);
     return this->checkWrite(this->addr_NodeID, node_id, this->NodeID);
   }
+  bool saveDatabaseUrl(String url)
+  {
+    this->maxCharacter = 120;
+    bool state = this->checkWrite(this->addr_databaseUrl, url, this->databaseUrl);
+    this->maxCharacter = 0;
+    return state;
+  }
 
   String readSSID()
   {
@@ -118,6 +130,13 @@ public:
   {
     return this->checkRead(this->addr_userID, this->userID);
   }
+  String readDatabaseUrl()
+  {
+    this->maxCharacter = 120;
+    String temp = this->checkRead(this->addr_databaseUrl, this->databaseUrl);
+    this->maxCharacter = 0;
+    return temp;
+  }
 
 private:
   int size = 1024;
@@ -129,6 +148,9 @@ private:
   int addr_userID = 100;
   String NodeID = "";
   int addr_NodeID = 150;
+  String databaseUrl = "";
+  int addr_databaseUrl = 200;
+  uint8_t maxCharacter = 0;
 
   String checkRead(int addr, String &cacheValue)
   {
@@ -151,10 +173,10 @@ private:
   {
     EEPROM.begin(this->size);
     String temp = "";
-    for (unsigned int i = addr; i < addr + 50; i++)
+    for (unsigned int i = addr; i < this->maxCharacter || (addr + 50); i++)
     {
       uint8_t character = EEPROM.read(i);
-      if (character != NULL)
+      if (character <= 126 && character >= 32)
       {
         temp = String(temp + (char)character);
       }
@@ -167,10 +189,10 @@ private:
     return temp;
   }
 
-  bool writeKey(int addr, String tmp)
+  bool writeKey(int addr, String tmp, uint8_t limit = 50)
   {
     int len_str = tmp.length();
-    if (len_str > 50)
+    if (len_str > this->maxCharacter || limit)
     {
       return false;
     }
@@ -207,14 +229,22 @@ private:
 
   void updateDatabaseNode(String uid, String nid)
   {
-    this->DATABASE_NODE = String("/user-" + uid + "/nodes/" + nid);
+    this->DATABASE_NODE = String(this->databaseUrl + "/user-" + uid + "/nodes/" + nid);
+    if (this->readNodeID().length() > 0 && this->readUserID().length() > 0 && this->readDatabaseUrl().length() > 0)
+    {
+      this->canAccess = true;
+    }
+    else
+    {
+      this->canAccess = false;
+    }
   }
 };
 
 // [********* INSTANCE *********]
 
 // => EEPROM
-EepromMiru eeprom(200);
+EepromMiru eeprom(400, "esp8266-device-db-default-rtdb.firebaseio.com");
 
 // => SERVER
 ESP8266WebServer server(80);
@@ -250,6 +280,7 @@ void setup()
   Serial.begin(115200);
   Serial.println("");
   Serial.println("[---PROGRAM START---]");
+  viewEEPROM();
 #endif
 
   LittleFS.begin();
@@ -276,6 +307,22 @@ void loop()
   // put your main code here, to run repeatedly:
   runner.execute();
 }
+
+#ifdef _DEBUG_
+void viewEEPROM()
+{
+  String ssid = eeprom.readSSID();
+  String password = eeprom.readPassword();
+  String url = eeprom.readDatabaseUrl();
+  String nodeID = eeprom.readNodeID();
+  String userID = eeprom.readUserID();
+  Serial.println("WIFI NAME = " + ssid + " | length = " + String(ssid.length()));
+  Serial.println("WIFI PASS = " + password + " | length = " + String(password.length()));
+  Serial.println("WIFI DATATBASE URL = " + url + " | length = " + String(url.length()));
+  Serial.println("WIFI NODE ID = " + nodeID + " | length = " + String(nodeID.length()));
+  Serial.println("WIFI USER ID = " + userID + " | length = " + String(userID.length()));
+}
+#endif
 
 void checkFirebaseInit()
 {
